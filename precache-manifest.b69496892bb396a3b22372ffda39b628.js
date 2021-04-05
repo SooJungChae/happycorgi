1deb6b5ad4e833899da44976040180aa41749582
ChaeSooJung <naan_ace@naver.com>
2021-04-05 12:58:40 +0900

    Updates

---
+++ precache-manifest.b69496892bb396a3b22372ffda39b628.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b16db688765ed78eabe5902058242d5e",
+    "url": "/happycorgi/index.html"
+  },
+  {
+    "revision": "f14daa45f1865cbcd207",
+    "url": "/happycorgi/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6d59211771e21fd836ea",
+    "url": "/happycorgi/static/js/2.83e4b106.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/happycorgi/static/js/2.83e4b106.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f14daa45f1865cbcd207",
+    "url": "/happycorgi/static/js/main.7da5cf3c.chunk.js"
+  },
+  {
+    "revision": "f5c2f1c6036a1a0ef226",
+    "url": "/happycorgi/static/js/runtime-main.87f2e40c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/happycorgi/static/media/logo.5d5d9eef.svg"
+  }
+]);
