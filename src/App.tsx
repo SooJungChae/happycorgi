73b9b5f8bc8eddaddc9a5d66ec3001745db1697c
ChaeSooJung <naan_ace@naver.com>
2021-04-06 17:08:09 +0900

    Edit Hoem, About style

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Home from "./page/Home";
 import About from "./page/About";
-import Navigation from "./components/Navigation";
 import './assets/css/style.css';
 
 function App() {
   return (
       <BrowserRouter basename={window.location.pathname || ''}>
       {/*<BrowserRouter basename={process.env.PUBLIC_URL}>*/}
-          <Navigation />
-          <Route exact path="/" component={Home}/>
-          <Route path="/about" component={About}/>
+          <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/about" component={About}/>
+          </Switch>
       </BrowserRouter>
   );
 }
--- src/page/About.tsx
+++ src/page/About.tsx
@@ -1,26 +1,25 @@
-import React, {useEffect} from 'react';
-import {appendScript} from "../utils/script";
+import {useEffect} from 'react';
 import * as THREE from 'three';
 
 let scene: any, camera: any, renderer: any, cube: any;
 
 const About = () => {
-    function animate() {
-        requestAnimationFrame( animate );
-
-        // cube.rotation.x -= 0.01;
-        cube.rotation.y += 0.02;
-
-        renderer.render( scene, camera );
-    }
-
     useEffect(() => {
+        function animate() {
+            requestAnimationFrame( animate );
+
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.02;
+
+            renderer.render( scene, camera );
+        }
+
         scene = new THREE.Scene();
         camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.4, 1000 );
 
         renderer = new THREE.WebGLRenderer();
         renderer.setSize( window.innerWidth, window.innerHeight );
-        document.body.appendChild( renderer.domElement );
+        document.getElementById('root')?.appendChild(renderer.domElement);
 
         const geometry = new THREE.BoxGeometry();
         const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -33,13 +32,7 @@ const About = () => {
 
     }, []);
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                About me
-            </header>
-        </div>
-    );
+    return null;
 };
 
 export default About;
