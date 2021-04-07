30d5b77f8ed50228e22ad9ec70ad5a1799a5048b
ChaeSooJung <naan_ace@naver.com>
2021-04-07 17:17:56 +0900

    Updates

---
+++ precache-manifest.5d3c93f299e1bd5fea0cd14c3a412053.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c67f5880085fd32e8b9bee3587b3bf3",
+    "url": "/happycorgi/index.html"
+  },
+  {
+    "revision": "5fa40964e9378e765758",
+    "url": "/happycorgi/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a055af47985126e54b3e",
+    "url": "/happycorgi/static/js/2.0754e71f.chunk.js"
+  },
+  {
+    "revision": "6c46089e713fdf01ec7e89132bc85450",
+    "url": "/happycorgi/static/js/2.0754e71f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5fa40964e9378e765758",
+    "url": "/happycorgi/static/js/main.533cef29.chunk.js"
+  },
+  {
+    "revision": "f5c2f1c6036a1a0ef226",
+    "url": "/happycorgi/static/js/runtime-main.87f2e40c.js"
+  }
+]);
