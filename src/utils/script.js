c29da791a55c5ea3d9e456af2644a5ec758d7034
SooJungChae <naan_ace@naver.com>
2021-04-12 05:27:41 +0000

    Updates

--- src/utils/script.js
+++
@@ -1,6 +0,0 @@
-export const appendScript = (url) => {
-    const script = document.createElement('script');
-    script.src = url;
-    script.async = true;
-    document.body.appendChild(script);
-}
